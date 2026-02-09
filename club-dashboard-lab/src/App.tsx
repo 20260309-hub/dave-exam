9b846584f48f8a95e7b4e03fa5e77fb40d5cc145
Mark Baladad <baladad_m@f112.ub.lan>
2026-02-09 08:52:10 +0800

    Initial Commit

---
+++ club-dashboard-lab/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  type MemberProps = {
+    name: string;
+    course: string;
+    status: string;
+    email: string;
+    phone: string;
+    activities: string[];
+    };
+  return (
+    <>
+    <h1>University Club Member Dashboard</h1>
+    </>
+  )
+}
+
+export default App
